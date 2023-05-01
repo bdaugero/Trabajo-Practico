bc735fbf75dda6d21cc38885714d1d5439574c68
Bautista <bdaugero@gmail.com>
2023-05-01 14:46:30 -0300

    Todo lo que hice ayer

--- archio-de-pruebas.hs
+++ archio-de-pruebas.hs
@@ -1,3 +1,48 @@
+
+-- Ejemplos
+
+usuario1 = (1, "Juan")
+usuario2 = (2, "Natalia")
+usuario3 = (3, "Pedro")
+usuario4 = (4, "Mariela")
+usuario5 = (5, "Natalia")
+
+relacion1_2 = (usuario1, usuario2)
+relacion1_3 = (usuario1, usuario3)
+relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los usuarios es indistinto
+relacion2_3 = (usuario3, usuario2)
+relacion2_4 = (usuario2, usuario4)
+relacion3_4 = (usuario4, usuario3)
+
+publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
+publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
+publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
+publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
+publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
+
+publicacion2_1 = (usuario2, "Hello World", [usuario4])
+publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
+
+publicacion3_1 = (usuario3, "Lorem Ipsum", [])
+publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
+publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
+
+publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
+publicacion4_2 = (usuario4, "I am Bob", [])
+publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
+
+
+usuariosA = [usuario1, usuario2, usuario3, usuario4]
+relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
+publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
+redA = (usuariosA, relacionesA, publicacionesA)
+
+usuariosB = [usuario1, usuario2, usuario3, usuario5]
+relacionesB = [relacion1_2, relacion2_3]
+publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
+redB = (usuariosB, relacionesB, publicacionesB)
+
+
 -- Intento de Ejercicio 1
 
 type Usuario = (Integer, String) -- (id, nombre)
@@ -9,8 +54,25 @@ type RedSocial = ([Usuario], [Relacion], [Publicacion])
 idDeUsuario :: Usuario -> Integer
 idDeUsuario (id, _) = id 
 
-noHayRelacionesRepetidas :: [Relacion] -> Bool
-noHayRelacionesRepetidas [(x)]  = [fst(x) /= sn()]
+perteneceLaRelacionALaListaDeUsuarios :: Relacion -> [Usuario] -> Bool
+perteneceLaRelacionALaListaDeUsuarios relacion (y:ys) = pertenece (fst relacion) (y:ys) && pertenece (snd relacion) (y:ys)
+
+estanLosUsuariosDeLaRelacionEnLaListaDeUsuarios ::  [Relacion] -> [Usuario] -> Bool
+estanLosUsuariosDeLaRelacionEnLaListaDeUsuarios [] _ = True
+estanLosUsuariosDeLaRelacionEnLaListaDeUsuarios (x:xs) (y:ys) | perteneceLaRelacionALaListaDeUsuarios x (y:ys) = estanLosUsuariosDeLaRelacionEnLaListaDeUsuarios xs (y:ys)
+                                                              | otherwise = False
+
+sonTodasLasRelacionesDiferentes :: [Relacion] -> Bool
+sonTodasLasRelacionesDiferentes [] = True
+sonTodasLasRelacionesDiferentes (x:xs) | pertenece x (xs) = False
+                                       | otherwise = sonTodasLasRelacionesDiferentes xs
+
+usuariosDeRelacionValidos :: [Relacion] -> [Usuario] -> Bool
+usuariosDeRelacionValidos x y =  sonTodasLasRelacionesDiferentes x && estanLosUsuariosDeLaRelacionEnLaListaDeUsuarios x y
+
+
+--noHayRelacionesRepetidas :: [Relacion] -> Bool
+--noHayRelacionesRepetidas [(x)]  = [fst(x) /= sn()]
 
 nombreDeUsuario :: Usuario -> String
 nombreDeUsuario (_, nombre) = nombre 
@@ -23,6 +85,7 @@ todosDistintos (x:xs) = not (pertenece x xs) && todosDistintos xs
 largo :: (Eq t) => [t] -> Int
 largo [] = 0
 largo (x:xs) = largo xs + 1
+
 
 pertenece :: (Eq t) => t -> [t] -> Bool
 pertenece _ [] = False
