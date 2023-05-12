67785fc9529079d07298da734aedadd4d7f516b8
Bautista <bdaugero@gmail.com>
2023-05-12 18:15:27 -0300

    Mas casos de red Vacia de los ejercios y redVacia creada en Testtp

--- Testtp.hs
+++ Testtp.hs
@@ -8,13 +8,13 @@ import Tp1
 run = runTestTT testSuiteEj1
 
 testSuiteEj1 = test [
-    "Caso 1: Red vacía" ~: (nombresDeUsuarios redTest1) ~?= [],
+    "Caso 1: Red vacía" ~: (nombresDeUsuarios redVacia) ~?= [],
     "Caso 2: Red con un usuario" ~: (nombresDeUsuarios redTest2) ~?= ["Juan"],
     "Caso 3: Red con nombres de usuarios repetidos" ~: (nombresDeUsuarios redTest3) ~?= ["Juan", "Maria"],
     "Caso 4: Red con nombres de usuarios que no se repiten" ~: (nombresDeUsuarios redTest4) ~?= ["Juan", "Julia", "Maria"]
         ]
 
-redTest1 = ([], [], [])
+redVacia = ([], [], [])
 redTest2 = ([(1,"Juan")], [], [])
 redTest3 = ([(1,"Juan"), (2, "Juan"), (3, "Maria")], [], [])
 redTest4 = ([(1,"Juan"), (2, "Julia"), (3, "Maria")], [], [])
