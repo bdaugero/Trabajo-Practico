409f8368d120b747a5b3f84af08dd24f7bb987d5
Bautista <bdaugero@gmail.com>
2023-05-12 18:20:50 -0300

    Algunos cambios en Testtp y cambio de nombres del archivo de pruebas para que funcione

--- Testtp.hs
+++ Testtp.hs
@@ -5,16 +5,21 @@ import Tp1
 
 -- Casos de Test
 
-run = runTestTT testSuiteEj1
+redTest2 = ([(1,"Juan")], [], [])
+redTest3 = ([(1,"Juan"), (2, "Juan"), (3, "Maria")], [], [])
+redTest4 = ([(1,"Juan"), (2, "Julia"), (3, "Maria")], [], [])
 
-testSuiteEj1 = test [
+
+-- Los mas usados
+redVacia = ([], [], [])
+
+
+run = runTestTT testSuiteEjercicio1
+
+testSuiteEjercicio1 = test [
     "Caso 1: Red vacía" ~: (nombresDeUsuarios redVacia) ~?= [],
     "Caso 2: Red con un usuario" ~: (nombresDeUsuarios redTest2) ~?= ["Juan"],
     "Caso 3: Red con nombres de usuarios repetidos" ~: (nombresDeUsuarios redTest3) ~?= ["Juan", "Maria"],
     "Caso 4: Red con nombres de usuarios que no se repiten" ~: (nombresDeUsuarios redTest4) ~?= ["Juan", "Julia", "Maria"]
         ]
 
-redVacia = ([], [], [])
-redTest2 = ([(1,"Juan")], [], [])
-redTest3 = ([(1,"Juan"), (2, "Juan"), (3, "Maria")], [], [])
-redTest4 = ([(1,"Juan"), (2, "Julia"), (3, "Maria")], [], [])
